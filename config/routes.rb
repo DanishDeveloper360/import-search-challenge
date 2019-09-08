5b2173098e0ac8656e47eb152aa1d2abf31ec5fd
DanishDeveloper360 <md.dan0@gmail.com>
2019-09-08 23:13:42 +0200

    setting up user login credentials for authentication with specs

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  post 'auth/login', to: 'authentication#authenticate'
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+  let(:user) { build(:user) }
+  let(:headers) { valid_headers.except('Authorization') }
+  let(:valid_attributes) do
+    attributes_for(:user, password_confirmation: user.password)
+  end
+
+  # User signup test suite
+  describe 'POST /signup' do
+    context 'when valid request' do
+      before { post '/signup', params: valid_attributes.to_json, headers: headers }
+
+      it 'creates a new user' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns success message' do
+        expect(json['message']).to match(/Account created successfully/)
+      end
+
+    end
+
+  end
+end

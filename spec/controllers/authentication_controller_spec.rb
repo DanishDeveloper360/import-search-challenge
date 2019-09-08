509b4ffe3f323a785d260e55c347f3ab85a2273f
DanishDeveloper360 <md.dan0@gmail.com>
2019-09-08 23:38:43 +0200

    spec fix

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -1,5 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe AuthenticationController, type: :controller do
+RSpec.describe ApplicationController, type: :controller do
+  # create test user
+  let!(:user) { create(:user) }
+   # set headers for authorization
+  let(:headers) { { 'Authorization' => token_generator(user.id) } }
+  let(:invalid_headers) { { 'Authorization' => nil } }
 
+  describe "#authorize_request" do
+    context "when auth token is passed" do
+      before { allow(request).to receive(:headers).and_return(headers) }
+
+      # private method authorize_request returns current user
+      it "sets the current user" do
+        expect(subject.instance_eval { authorize_request }).to eq(user)
+      end
+    end
+  end
 end

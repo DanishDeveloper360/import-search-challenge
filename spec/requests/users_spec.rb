9d18d0a8c929045a83ee51a9bc6d5d628c3b9ec0
DanishDeveloper360 <md.dan0@gmail.com>
2019-09-08 23:23:20 +0200

    authentication service class with specs | user specs updated | bug fixes |

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,7 +20,22 @@ RSpec.describe 'Users API', type: :request do
         expect(json['message']).to match(/Account created successfully/)
       end
 
+      it 'returns an authentication token' do
+        expect(json['auth_token']).not_to be_nil
+      end
     end
 
+    context 'when invalid request' do
+      before { post '/signup', params: {}, headers: headers }
+
+      it 'does not create a new user' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns failure message' do
+        expect(json['message'])
+          .to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
+      end
+    end
   end
 end

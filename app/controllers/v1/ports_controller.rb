c18ed325be5348b5a4f16ac2214058421eba2abf
DanishDeveloper360 <md.dan0@gmail.com>
2019-09-09 04:14:33 +0200

    refactor code

--- app/controllers/v1/ports_controller.rb
+++ app/controllers/v1/ports_controller.rb
@@ -15,35 +15,9 @@ module V1
         elsif params[:file].content_type != "text/csv"
           return json_response({ message: Message.unsupported_media_type }, :unsupported_media_type)
         end
-
         file_path = params[:file].path
-        ports = []
-        portTypes = PortType.all.select(:id, :name).take(10)
-        
-        CSV.foreach(file_path, headers: true) do |row|
-
-          raise(ExceptionHandler::HeaderCorrupt, Message.header_corrupt) unless ['Name', 'Code', 'City', 'Port type', 'Oceans insights code','Latitude', 'Longitude', 'Bigschedules' ].all? { |header| row.headers.include? header }
-
-          propTypeForRow = portTypes.select {|e| e[:name] == row['Port type']}
-
-          if propTypeForRow.count <= 0
-            PortType.create!(:name => row['Port type'], 
-              :created_at => row['Created at'].blank? ? Date.today() : DateTime.parse(row['Created at']), 
-              :updated_at => row['Updated at'].blank? ? Date.today() : DateTime.parse(row['Updated at']))
-            portTypes = PortType.all.select(:id, :name).take(10)
-            propTypeForRow = portTypes.select {|e| e[:name] == row['Port type']}
-          end
-          newPort = { :name => row['Name'], :code => row['Code'], :city => row['City'], 
-          :oceans_insights_code => row['Oceans insights code'], :lat => row['Latitude'], :lng => row['Longitude'], :big_schedules => row['Bigschedules'], 
-          :created_at => row['Created at'].blank? ? Date.today() : DateTime.parse(row['Created at']), 
-          :updated_at => row['Updated at'].blank? ? Date.today() : DateTime.parse(row['Updated at']),
-          :port_type_id => propTypeForRow[0][:id] }
-          ports << newPort
-        end
-
-        Port.import ports, validate: false, on_duplicate_key_update: { conflict_target: [:code], columns: [ :name, :city, :lat, :lng, :port_type_id, :big_schedules, :oceans_insights_code ] }
-        
-        json_response({ message: 'Ports uploaded successfully', count: ports.count }, :created)
+        imported_count = Port.ImportCSV(file_path)
+        json_response({ message: 'Ports uploaded successfully', count: imported_count }, :created)
       end
     
       # GET /ports/:id
@@ -67,8 +41,8 @@ module V1
         end
 
         if(params[:code])
-          @port = Port.find_by_code(params[:code]).paginate(page: params[:page], per_page: params[:pageSize])
-
+          @port = Port.find_by_code(params[:code])
+          
         elsif(params[:name])          
           @port = Port.where("name LIKE :query", query: "%#{params[:name]}%").paginate(page: params[:page], per_page: params[:pageSize])
             
--- app/models/port.rb
+++ app/models/port.rb
@@ -9,5 +9,34 @@ class Port < ApplicationRecord
     Port.joins(:port_type).select("ports.*, port_types.name as port_type_name")
   end
   
+  def self.ImportCSV(file_path)
+    ports = []
+    portTypes = PortType.all.select(:id, :name).take(10)
+    # file_path = inputs[:file_path]
+    CSV.foreach(file_path, headers: true) do |row|
+
+      raise(ExceptionHandler::HeaderCorrupt, Message.header_corrupt) unless ['Name', 'Code', 'City', 'Port type', 'Oceans insights code','Latitude', 'Longitude', 'Bigschedules' ].all? { |header| row.headers.include? header }
+
+      propTypeForRow = portTypes.select {|e| e[:name] == row['Port type']}
+
+      if propTypeForRow.count <= 0
+        PortType.create!(:name => row['Port type'], 
+          :created_at => row['Created at'].blank? ? Date.today() : DateTime.parse(row['Created at']), 
+          :updated_at => row['Updated at'].blank? ? Date.today() : DateTime.parse(row['Updated at']))
+        portTypes = PortType.all.select(:id, :name).take(10)
+        propTypeForRow = portTypes.select {|e| e[:name] == row['Port type']}
+      end
+      newPort = { :name => row['Name'], :code => row['Code'], :city => row['City'], 
+      :oceans_insights_code => row['Oceans insights code'], :lat => row['Latitude'], :lng => row['Longitude'], :big_schedules => row['Bigschedules'], 
+      :created_at => row['Created at'].blank? ? Date.today() : DateTime.parse(row['Created at']), 
+      :updated_at => row['Updated at'].blank? ? Date.today() : DateTime.parse(row['Updated at']),
+      :port_type_id => propTypeForRow[0][:id] }
+      ports << newPort
+    end
+
+    Port.import ports, validate: false, on_duplicate_key_update: { conflict_target: [:code], columns: [ :name, :city, :lat, :lng, :port_type_id, :big_schedules, :oceans_insights_code ] }
+    
+    return ports.count
+  end
 
 end
